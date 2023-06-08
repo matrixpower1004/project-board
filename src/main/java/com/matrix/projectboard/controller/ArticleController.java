534b9dba82ebe8cfaba4426392c9640526c75ee5
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-09 03:48:47 +0900

    #2 - 게시글 관련 View 테스트 작성
    우선 어떤 경로로 View를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/matrix/projectboard/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package com.matrix.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-06-09
+ * description    :
+ */
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+}
---
+++ src/test/java/com/matrix/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,79 @@
+package com.matrix.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-06-09
+ * description    :
+ */
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+        // 이 때는 게시글 데이터가 model attribute 에 넘겨져야 한다.
+    }
+
+    // 게시판 페이지에서 검색 바를 밑에 넣을 거라 이 테스트가 필요 없을 수 있다.
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        // 검색하면 목록외에 게시글에 대한 데이터는 없어야 한다.
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        // 검색하면 목록외에 게시글에 대한 데이터는 없어야 한다.
+    }
+
+} // end of class
