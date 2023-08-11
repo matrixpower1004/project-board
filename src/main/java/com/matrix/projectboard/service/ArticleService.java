913c7e8e65454b8f5e9becbdf39efd3a84673c0c
matrixpower1004 <matrixpower1004@gmail.com>
2023-08-11 18:47:17 +0900

    Feat #21 - 게시판 서비스 뷰 기능 정의 및 테스트 작성
    * 게시판 페이지 기능 테스트 정의
    * 게시글 페이지 기능 테스트 정의

---
+++ src/main/java/com/matrix/projectboard/service/ArticleService.java
@@ -0,0 +1,44 @@
+package com.matrix.projectboard.service;
+
+import com.matrix.projectboard.domain.type.SearchType;
+import com.matrix.projectboard.dto.ArticleDto;
+import com.matrix.projectboard.dto.ArticleWithCommentsDto;
+import com.matrix.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-08-10
+ * description    :
+ */
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(ArticleDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+
+}
